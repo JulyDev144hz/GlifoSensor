8a4937f5d81e09cf74ffd8b7751547e044aeb2bc
facunare <arechaga.facundoet36@gmail.com>
2023-03-28 10:57:06 -0300

    responsive, botoncitos y mas cosas!!

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([-34.6140305,-58.4517207], 12);
+var map = L.map('map').setView([-34.6140305, -58.4517207], 12);
 const todo = document.querySelector('.todo')
 const mapa = document.querySelector('#map')
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,23 +7,30 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 let arrayMarkers = []
-let marker 
+let marker
 const getSensors = async () => {
+
+    oldArrayMarkers = arrayMarkers
     
+
     arrayMarkers = []
+
+
+
+
+
 
     let data = await fetch("http://localhost:3000/sensor")
     let json = await data.json()
     json.forEach(s => {
-        if(s.humedad < 20){
-
+        if (s.humedad < 20) {
             marker = L.circle(s.coords, {
                 color: 'green',
                 fillColor: 'green',
                 fillOpacity: 0.5,
                 radius: 500
             })
-        }else{
+        } else {
             marker = L.circle(s.coords, {
                 color: 'red',
                 fillColor: '#f03',
@@ -31,23 +38,42 @@ const getSensors = async () => {
                 radius: 500
             })
         }
+
+
+        
         marker.addTo(map);
-
-        marker.on("click", ()=>{
+        
+        marker.on("mouseover", () => {
             updateData(s.name, s.temperatura, s.humedad)
-            
-            console.log("hola")
             todo.style.display = "block"
-
-           
+            
+            
+            
         })
-       
-
+        
+        
+        
+        
+        
+        
         arrayMarkers.push(marker)
+
+
     });
+
+    oldArrayMarkers.map(mk => {
+    
+    
+        // mk.setLatLng(new L.LatLng(mk.getLatLng().lat, mk.getLatLng().lng));
+        
+        mk.removeFrom(map)
+    })
+    
+    oldArrayMarkers = []
+
 }
 
-const updateData = (nombre, temperatura, humedad) =>{
+const updateData = (nombre, temperatura, humedad) => {
     let dataNombre = document.getElementById("name")
     let dataTemperatura = document.getElementById("temp")
     let dataHumedad = document.getElementById('hum')
@@ -65,6 +91,6 @@ setInterval(() => {
 const xButton = document.querySelector('.xButton')
 
 
-xButton.addEventListener('click', ()=>{
+xButton.addEventListener('click', () => {
     todo.style.display = "none"
 })
