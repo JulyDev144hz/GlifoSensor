9b3cfff4b470daf510c04d06224c4b6837e916a2
julydev144hz <ippolito.julianet36@gmail.com>
2023-06-27 10:09:40 -0300

    funcionan los charts de los sensores y se actualizan en tiempo real

--- frontend/main.js
+++ frontend/main.js
@@ -8,8 +8,11 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 const sensores = document.getElementById("sensores");
+let chartSensors = [];
+
 const ctx = document.getElementById("ChartPromedio");
-const cfg = {
+
+const defaultcfg = {
   type: "line",
   data: {
     datasets: [
@@ -22,7 +25,7 @@ const cfg = {
         tension: 0.4,
       },
       {
-        label: "Co2",
+        label: "CO2",
         data: [
           { x: "2016-12-25", y: 20 },
           { x: "2016-12-26", y: 15 },
@@ -40,7 +43,7 @@ const cfg = {
     ],
   },
 };
-let chart1 = new Chart(ctx, cfg);
+let chart1 = new Chart(ctx, defaultcfg);
 // [0,1,2,3,4,5,6].map(e=>{
 //   humedad.data.datasets[0].data[e] = {x:"2016/12/"+e,y:e*50}
 
@@ -110,29 +113,55 @@ setTimeout(() => {
       window.location = "#datosSensor";
       try {
         sensores.innerHTML = "";
+        chartSensors = []
         barrio.sensores.map(async (sns, indx) => {
           let id = sns._id;
           sensores.innerHTML += `
           <div class="sensor">
-            <h6>${sns.name}</h6>
-            <canvas id="chartSensor${indx+1}"></canvas>
+            <h6>Sensor: ${sns.name}</h6>
+            <canvas id="chartSensor${indx + 1}"></canvas>
           </div>`;
+          let config = {
+            type: "line",
+            data: {
+              datasets: [
+                {
+                  label: "Humedad",
+                  data: [],
+                  tension: 0.4,
+                },
+                {
+                  label: "CO2",
+                  data: [],
+                  tension: 0.4,
+                },
+                {
+                  label: "Temperatura",
+                  data: [],
+                  tension: 0.4,
+                },
+              ],
+            },
+          };
+          config.data.datasets[0].data, config.data.datasets[1].data, config.data.datasets[2].data = []
 
-          setTimeout(() => {
-            new Chart(document.getElementById("chartSensor"+(indx+1)), cfg)
-
-          }, (indx+1)*1);
 
           fetch("http://localhost:3000/historySensor/" + id)
             .then((data) => data.json())
             .then((json) => {
-              // json.map((sensor, index) => {
-              //   config.data.datasets[0].data[index] = { x: sensor.timeStamp, y: sensor.humedad }
-              //   config.data.datasets[1].data[index] = { x: sensor.timeStamp, y: sensor.co2 }
-              //   config.data.datasets[2].data[index] = { x: sensor.timeStamp, y: sensor.temperatura }
-              // })
-              // if (json.length <2 ){
-              // }
+
+              json.map((sensor, index) => {
+                config.data.datasets[0].data[index] = { x: sensor.timeStamp, y: sensor.humedad }
+                config.data.datasets[1].data[index] = { x: sensor.timeStamp, y: sensor.co2 }
+                config.data.datasets[2].data[index] = { x: sensor.timeStamp, y: sensor.temperatura }
+              })
+
+              // setTimeout(() => {
+              let chart = new Chart(document.getElementById("chartSensor" + (indx + 1)), config)
+
+              chartSensors.push({ chart: chart, id: id })
+              chart.update()
+              // }, 200);
             });
         });
       } catch (error) {
@@ -156,9 +185,21 @@ setTimeout(() => {
 
 const getSensors = async () => {
   try {
-    oldArrayMarkers = arrayMarkers;
 
-    arrayMarkers = [];
+
+    let dataHistory = await fetch("http://localhost:3000/historySensor");
+    let jsonHistory = await dataHistory.json()
+    chartSensors.map((sensorChart, index) => {
+      sensorChart.chart.config.data.datasets[0].data = []
+      sensorChart.chart.config.data.datasets[1].data = []
+      sensorChart.chart.config.data.datasets[2].data = []
+      jsonHistory.filter(x => x.idSensor == sensorChart.id).map((sensor, index) => {
+        sensorChart.chart.config.data.datasets[0].data[index] = { x: sensor.timeStamp, y: sensor.humedad }
+        sensorChart.chart.config.data.datasets[1].data[index] = { x: sensor.timeStamp, y: sensor.co2 }
+        sensorChart.chart.config.data.datasets[2].data[index] = { x: sensor.timeStamp, y: sensor.temperatura }
+      })
+      sensorChart.chart.update()
+    })
 
     let data = await fetch("http://localhost:3000/sensor");
     let json = await data.json();
@@ -177,11 +218,6 @@ const getSensors = async () => {
       });
     });
 
-    oldArrayMarkers.map((mk) => {
-      mk.removeFrom(map);
-    });
-
-    oldArrayMarkers = [];
   } catch (error) {
     console.error(error);
   }
